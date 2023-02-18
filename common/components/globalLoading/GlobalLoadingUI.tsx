7245526ab61860740c18973bb44a31c75d96919c
Ian Lai <ian.lai@houzz.com>
2023-02-19 02:59:30 +0800

    style: set global loader mobile style

--- common/components/globalLoading/GlobalLoadingUI.tsx
+++ common/components/globalLoading/GlobalLoadingUI.tsx
@@ -3,7 +3,7 @@ import { Image, chakra, forwardRef } from '@chakra-ui/react';
 const GlobalLoadingUI = forwardRef<{}, 'div'>((_, ref) => {
     return (
         <chakra.div ref={ref} top="0" left="0" pos="fixed" zIndex="popover" w="100vw" h="100vh" backgroundColor="gray.700">
-            <Image src="/loading.gif" alt="loading" w={[60, 96]} pos="absolute" right={-8} bottom={-8} />
+            <Image src="/loading.gif" alt="loading" w={[60, 96]} pos="absolute" right={-8} bottom={[16, -8]} />
         </chakra.div>
     );
 });
